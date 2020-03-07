7ea4c34b784a5e226bb4fa5d5fa6ba8746011e95
Jian Yuan Lee <jianyuan@gmail.com>
2020-03-07 00:46:40 +0000

    Implement handshake

--- terraform/plugin.py
+++ terraform/plugin.py
@@ -1,11 +1,19 @@
 import asyncio
+import base64
 import collections
+import contextlib
 import logging
 import os
+import ssl
 import sys
+import tempfile
 import typing
 
-from terraform import settings
+import grpclib.server
+from grpclib.utils import graceful_exit
+
+from terraform import settings, utils
+from terraform.grpc_controller import GRPCController
 
 logger = logging.getLogger(__name__)
 
@@ -62,6 +70,42 @@ class Provider:
         self.data_sources.add(data_source)
 
 
+async def write_handshake_response(
+    *,
+    file: typing.TextIO,
+    core_protocol_version: int = settings.CORE_PROTOCOL_VERSION,
+    protocol_version: int,
+    port: int,
+    certificate,
+):
+    """
+    Write the protocol versions, service name, address and certificate to the IO
+    for the client.
+    """
+    certificate_der = utils.encode_certificate_der(certificate)
+    certificate_b64 = base64.b64encode(certificate_der).decode("ascii").rstrip("=")
+
+    file.write(
+        "{}|{}|{}|{}|{}|{}\n".format(
+            core_protocol_version,
+            protocol_version,
+            "tcp",
+            f"127.0.0.1:{port}",
+            "grpc",
+            certificate_b64,
+        )
+    )
+    file.flush()
+
+
+async def wait_shutdown_event(
+    *, server: grpclib.server.Server, shutdown_event: asyncio.Event
+):
+    """Wait for a shutdown event to close the server."""
+    await shutdown_event.wait()
+    server.close()
+
+
 async def run_server(*, provider: Provider):
     if os.getenv(settings.MAGIC_COOKIE_KEY) != settings.MAGIC_COOKIE_VALUE:
         logger.error(
@@ -75,6 +119,47 @@ async def run_server(*, provider: Provider):
     if min_port > max_port:
         logger.error("PLUGIN_MIN_PORT value is greater than PLUGIN_MAX_PORT value")
         sys.exit(1)
+
+    certificate_data = utils.generate_certificate()
+    with contextlib.ExitStack() as stack:
+        keyfile = stack.enter_context(tempfile.NamedTemporaryFile())
+        keyfile.write(utils.encode_private_key(certificate_data.private_key))
+        keyfile.flush()
+        certfile = stack.enter_context(tempfile.NamedTemporaryFile())
+        certfile.write(utils.encode_certificate_pem(certificate_data.certificate))
+        certfile.flush()
+
+        client_cert = os.getenv("PLUGIN_CLIENT_CERT")
+        ctx = ssl.SSLContext(ssl.PROTOCOL_TLS)
+        ctx.verify_mode = ssl.CERT_REQUIRED
+        ctx.minimum_version = ssl.TLSVersion.TLSv1_2
+        ctx.load_cert_chain(certfile=certfile.name, keyfile=keyfile.name)
+        ctx.load_verify_locations(cadata=client_cert)
+
+        shutdown_event = asyncio.Event()
+
+        handlers = [
+            GRPCController(shutdown_event=shutdown_event),
+        ]
+        server = grpclib.server.Server(handlers)
+        with graceful_exit([server]):
+            port = min_port
+            while port <= max_port:
+                try:
+                    await server.start("127.0.0.1", port, ssl=ctx)
+                except OSError:
+                    port += 1
+                else:
+                    break
+
+            await write_handshake_response(
+                file=sys.stdout,
+                protocol_version=settings.PROTOCOL_VERSION,
+                port=port,
+                certificate=certificate_data.certificate,
+            )
+            await wait_shutdown_event(server=server, shutdown_event=shutdown_event)
+            await server.wait_closed()
 
 
 def run(*, provider: Provider):
---
+++ terraform/utils.py
@@ -0,0 +1,84 @@
+import datetime
+import typing
+
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import ec
+
+
+class CertificatePrivateKey(typing.NamedTuple):
+    certificate: x509.Certificate
+    private_key: ec.EllipticCurvePrivateKeyWithSerialization
+
+
+def generate_certificate() -> CertificatePrivateKey:
+    key = typing.cast(
+        ec.EllipticCurvePrivateKeyWithSerialization,
+        ec.generate_private_key(curve=ec.SECP521R1(), backend=default_backend()),
+    )
+
+    subject = issuer = x509.Name(
+        [
+            x509.NameAttribute(
+                x509.oid.NameOID.ORGANIZATION_NAME, "python-terraform-plugin"
+            ),
+            x509.NameAttribute(x509.oid.NameOID.COMMON_NAME, "localhost"),
+        ]
+    )
+
+    certificate = (
+        x509.CertificateBuilder()
+        .subject_name(subject)
+        .issuer_name(issuer)
+        .public_key(key.public_key())
+        .serial_number(x509.random_serial_number())
+        .not_valid_before(datetime.datetime.utcnow())
+        .not_valid_after(datetime.datetime.utcnow() + datetime.timedelta(days=365))
+        .add_extension(
+            x509.KeyUsage(
+                digital_signature=True,
+                content_commitment=False,
+                key_encipherment=True,
+                data_encipherment=False,
+                key_agreement=True,
+                key_cert_sign=True,
+                crl_sign=False,
+                encipher_only=False,
+                decipher_only=False,
+            ),
+            critical=True,
+        )
+        .add_extension(
+            x509.ExtendedKeyUsage(
+                [
+                    x509.oid.ExtendedKeyUsageOID.CLIENT_AUTH,
+                    x509.oid.ExtendedKeyUsageOID.SERVER_AUTH,
+                ]
+            ),
+            critical=True,
+        )
+        .add_extension(x509.BasicConstraints(ca=True, path_length=None), critical=True)
+        .add_extension(
+            x509.SubjectAlternativeName([x509.DNSName("localhost")]), critical=False,
+        )
+        .sign(key, hashes.SHA256(), default_backend())
+    )
+
+    return CertificatePrivateKey(certificate=certificate, private_key=key)
+
+
+def encode_certificate_pem(certificate: x509.Certificate) -> bytes:
+    return certificate.public_bytes(serialization.Encoding.PEM)
+
+
+def encode_certificate_der(certificate: x509.Certificate) -> bytes:
+    return certificate.public_bytes(serialization.Encoding.DER)
+
+
+def encode_private_key(key: ec.EllipticCurvePrivateKeyWithSerialization) -> bytes:
+    return key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption(),
+    )
